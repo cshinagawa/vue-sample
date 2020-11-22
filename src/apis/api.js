fdff698a5be1fe3513c0c004c73c33feaf629921
c.shinagawa <c.shinagawa@wiz-net.jp>
2020-11-23 00:44:56 +0900

    :sparkles: API実装

---
+++ src/apis/api.js
@@ -0,0 +1,13 @@
+import axios from'axios'
+
+export const client = axios.create({
+    baseURL: 'http://localhost:3000'
+})
+
+// モック起動？
+
+export default {
+    getPosts(params) {
+        return client.get('articles', params);
+    }
+}
